013e08ac7cd566d6e8a0054fb4803dc41cb0c4e3
haydogsup <joshua.r.mays@gmail.com>
2014-10-17 16:52:54 -0500

    commenting out 'by ->user->email' til users table is added and events are associated with users. also changed some  to .

--- app/views/activities/index.blade.php
+++ app/views/activities/index.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.master')
- @include('partials.navbar')
- @stop
+
 @section('content')
  <div class="col-lg-12 col-md-12 col-sm-12">
 
     <h1>Seek City</h1>
-    <a class='btn btn-default' href={{ action('ActivitiesController@create') }}>New Entry</a>
+    <a class='btn btn-default' href="{{ action('ActivitiesController@create') }}">New Entry</a>
     <hr>
     <article> 
         @forelse($activities as $activity)
-        <h3>{{{ $this->title }}}</h3>
+        <h3>{{{ $activity->title }}}</h3>
 
-        <p><span class='glyphicon glyphicon-time'></span> {{{ $activity->updated_at->format(activity::DATE_FORMAT) }}} by {{{ $activity->user->email }}}</p>
+        <p><span class='glyphicon glyphicon-time'></span> {{{ $activity->updated_at->format(Activity::DATE_FORMAT) }}} <!--by --> {{-- $activity->user->email --}}</p>
         
-        <img class='img-responsive' src="{{ $this->img }}" alt="">
+        <img class='img-responsive' src="{{ $activity->img }}" alt="">
  
         
-        <p>{{{ $this->body }}}</p>
+        <p>{{{ $activity->body }}}</p>
         
         <a class="btn btn-sm btn-primary" href="activities/{{ $activity->id }}">More Info <span class="glyphicon glyphicon-chevron-right"></span></a>
         
@@ -28,6 +27,5 @@
 
 </div>
 @stop
-@include('partials.footer')
-@stop
+
 
