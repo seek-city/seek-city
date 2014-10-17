e12d8ed29384894ddfb4f5979f8ca091d2b3e7f1
haydogsup <joshua.r.mays@gmail.com>
2014-10-16 22:01:09 -0500

    changed environment detection.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,9 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(function()
-{
-    return !empty($_SERVER['LARAVEL_ENV']) ? $_SERVER['LARAVEL_ENV'] : 'production';
+$env = $app->detectEnvironment(function () {
+
+    return isset($_SERVER['LARAVEL_ENV']) && $_SERVER['LARAVEL_ENV'] == 'local' ? 'local' : 'production';
+
 });
 
 /*
