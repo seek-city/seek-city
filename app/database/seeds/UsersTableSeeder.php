44bc8a948ba61998df312b4abeca8c5b1b6783e5
haydogsup <joshua.r.mays@gmail.com>
2014-10-17 17:54:28 -0500

    Adding seeder for Users Table.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -1,20 +1,16 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
-
 class UsersTableSeeder extends Seeder {
 
-	public function run()
-	{
-		$faker = Faker::create();
-
-		foreach(range(1, 10) as $index)
-		{
-			User::create([
-
-			]);
-		}
-	}
-
+    public function run()
+    {
+        $user = new User();
+        $user->email = $_ENV['USER_EMAIL'];
+        $user->password = $_ENV['USER_PASSWORD'];
+        $user->username = $_ENV['USER_USERNAME'];
+        $user->image_path = 'http://lorempixel.com/200/200/';
+        $user->first_name = $_ENV['USER_FIRST_NAME'];
+        $user->last_name = $_ENV['USER_LAST_NAME'];
+        $user->save();
+    }
 }
