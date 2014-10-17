777594f623b8da327bfa7bc3bdae20a2e3373c3e
haydogsup <joshua.r.mays@gmail.com>
2014-10-17 17:13:29 -0500

    adding users table migration

--- app/database/migrations/2014_10_17_214630_create_users_table.php
+++ app/database/migrations/2014_10_17_214630_create_users_table.php
@@ -5,29 +5,37 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('password', 255);
+            $table->string('email', 200)->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('image_path');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
 
 }
