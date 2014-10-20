2c0f06693fbcf51a294f5933431fd6a99c4a5d19
haydogsup <joshua.r.mays@gmail.com>
2014-10-20 11:37:22 -0500

    Adding routes and controller actions for logging in.

---
+++ app/views/login.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        {{ Form::open(array('action' => 'HomeController@doLogin')) }}
+            <div class="form-group">
+                {{ Form::label('username', 'Username:') }}
+                {{ Form::text('username', Input::old('username'), array('class' => 'form-control', 'placeholder' => 'username')) }}
+            
+                {{ $errors->first('username', '<span class="alert alert-danger alert-dismissible help-block"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('password', 'Password:') }}
+                {{ Form::password('password', ['class' => 'form-control']) }}
+                
+                {{ $errors->first('password', '<span class="alert alert-danger alert-dismissible help-block"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button> :message</span>') }}
+            </div>
+            <div class="form-group">
+                {{ Form::submit('Sign In', array('class' => 'btn btn-md')) }}
+            </div>
+        {{ Form::close() }}
+    </div>
+</div>
+
+@stop
