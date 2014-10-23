fb4cc0618156957777f01f2314fad3654f795b44
haydogsup <joshua.r.mays@gmail.com>
2014-10-23 01:18:32 -0500

    moving nabber to partial

---
+++ app/views/partials/navbar.blade.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/activities">Seek City</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class='active'>
+                    {{ link_to_action('HomeController@index', 'Home'); }}
+                </li>
+                <li>
+                    <a href="/">About Seek City</a>
+                </li>
+                @if (Auth::check())
+                <li>
+                    {{ link_to_action('UsersController@show', Auth::user()->username, Auth::id()); }}
+                </li>
+                <li>
+                    {{ link_to_action('HomeController@doLogout', 'Log Out'); }}
+                </li>
+                @else
+                <li>
+                    {{ link_to_action('UsersController@create', 'Sign Up'); }}
+                </li>
+                <li>
+                    {{ link_to_action('HomeController@showLogin', 'Log In'); }}
+                </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- /.navbar -->
