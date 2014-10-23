7486261647017cc3bc110336f932b8ec1f2b7382
haydogsup <joshua.r.mays@gmail.com>
2014-10-23 00:35:34 -0500

    added title and well

--- app/views/categories/create.blade.php
+++ app/views/categories/create.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.master')
- @include('partials.navbar')
- @stop
- 
-@section('topscript')<title>Create an Category</title> @stop
+
+@section('title')
+Create an Category
+@stop
 
 @section('content')
-<div class='col-md-8'>
+<div class='col-md-8 col-md-offset-2 well'>
     <h2>Create a New Category</h2>
     <hr>
     {{ Form::open(['action' => 'CategoriesController@store', 'method' => 'POST', 'files' => true]) }}
 
         @include('categories.form')
-    
+
     <div class='form-group'>
         {{ Form::reset('Reset', ['class' => 'btn btn-default']) }}
         {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
     </div>
+
     {{ Form::close() }}
 </div>
-@stop
-@include('partials.footer')
 @stop
