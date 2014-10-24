f96aa0c81975eebf020d7b74f56c96c164cd4f5e
haydogsup <joshua.r.mays@gmail.com>
2014-10-24 11:49:45 -0500

    migrations for activity/mood and activity/category pivots. removing migration for user_id column in activities table.

---
+++ app/database/migrations/2014_10_23_215235_create_activity_mood_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateActivityMoodTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('activity_mood', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('activity_id')->unsigned()->index();
+			$table->foreign('activity_id')->references('id')->on('activities')->onDelete('cascade')->onUpdate('cascade');
+			$table->integer('mood_id')->unsigned()->index();
+			$table->foreign('mood_id')->references('id')->on('moods')->onDelete('cascade')->onUpdate('cascade');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('activity_mood');
+	}
+
+}
