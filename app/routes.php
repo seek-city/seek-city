3a3efdd3e169e748509ebae46e6bafe542052010
haydogsup <joshua.r.mays@gmail.com>
2014-10-26 21:30:32 -0500

    Adding Carbon Alias for easy use.

--- app/routes.php
+++ app/routes.php
@@ -22,6 +22,7 @@ Route::post('/login', 'HomeController@doLogin');
 // LOG USER OUT
 Route::get('/logout', 'HomeController@doLogout');
 
+
 // MANAGE CATEGORIES
 Route::get('/categories/manage', 'CategoriesController@manage');
 
