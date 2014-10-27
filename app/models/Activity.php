9297aba7cc6407b1892b2357759b14e0daf943a2
haydogsup <joshua.r.mays@gmail.com>
2014-10-26 21:49:51 -0500

    Updates to Activity Model: - requiring activity date - added user() relationship. - added getter/formatter method for price

--- app/models/Activity.php
+++ app/models/Activity.php
@@ -3,15 +3,13 @@
 class Activity extends BaseModel
 {
 
-    // Add your validation rules here
     public static $rules = [
         'title' => 'required|max:255',
         'body' => 'required',
-        // 'activity_date' => 'required',
+        'activity_date' => 'required',
         'price' => 'required'
     ];
 
-    // Don't forget to fill this array
     protected $fillable = [
         'title',
         'body',
@@ -31,14 +29,37 @@ class Activity extends BaseModel
 
     protected $table = 'activities';
 
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
     public function moods()
     {
         return $this->belongsToMany('Mood');
     }
-    
+
     public function categories()
     {
         return $this->belongsToMany('Category');
+    }
+
+    // DB STORES PRICE AS ARRAY VALS 0-4. 
+    // RETURN TO DISPLAY STRING REPRESENTATION.
+    public function getPrice()
+    {
+        switch($this->price){
+            case 0:
+                return 'Free';
+            case 1:
+                return '$';
+            case 2:
+                return '$$';
+            case 3:
+                return '$$$';
+            case 4:
+                return '$$$$';
+        }
     }
 }
 
