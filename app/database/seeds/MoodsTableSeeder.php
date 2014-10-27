71be137aff1318f7f4e2b8c536084d1a5ab68bcf
haydogsup <joshua.r.mays@gmail.com>
2014-10-26 21:48:24 -0500

    Migrations & Seeder Updates: - Adding user_id field to activities table. - Removing timestamps from activity_mood and activity_category tables. - Faking user_id for Activity Seeder. - Faking categories on Categories Seeder. - Faking moods on Moods Seeder. - Adding Moods and Categories Seeders to DB Seeder.

--- app/database/seeds/MoodsTableSeeder.php
+++ app/database/seeds/MoodsTableSeeder.php
@@ -1,20 +1,28 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
-
 class MoodsTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$faker = Faker::create();
-
-		foreach(range(1, 10) as $index)
-		{
-			Mood::create([
-
-			]);
-		}
+        $mood1 = new Mood();
+        $mood1->name = 'Artsy';
+        $mood1->save();
+        
+        $mood2 = new Mood();
+        $mood2->name = 'Classy';
+        $mood2->save();
+        
+        $mood3 = new Mood();
+        $mood3->name = 'Rainy Day';
+        $mood3->save();
+        
+        $mood4 = new Mood();
+        $mood4->name = 'Sporty';
+        $mood4->save();
+        
+        $mood5 = new Mood();
+        $mood5->name = 'Festive';
+        $mood5->save();
 	}
 
 }
