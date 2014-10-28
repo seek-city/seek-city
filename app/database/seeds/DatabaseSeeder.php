460baf89504b2585277003009f1c436a0dac77aa
philip james winrock <philipwinrock@aol.com>
2014-10-28 13:10:14 -0500

    venues seeder and commenting out other seeders.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,10 +11,11 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		$this->call('UsersTableSeeder');
-		$this->call('ActivitiesTableSeeder');
-		$this->call('CategoriesTableSeeder');
-		$this->call('MoodsTableSeeder');
+		// $this->call('UsersTableSeeder');
+		// $this->call('ActivitiesTableSeeder');
+		// $this->call('CategoriesTableSeeder');
+		// $this->call('MoodsTableSeeder');
+		$this->call('VenuesTableSeeder');
 	}
 
 }
