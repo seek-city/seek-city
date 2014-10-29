bac3e2826349877aa20c087528712e72db7e5f58
Ruth Spina <rbips.25@gmail.com>
2014-10-29 16:11:39 -0500

    updated index/activities and icons on main menu

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,6 +1,4 @@
 <script src="{{ asset('/js/jquery-2.1.1.min.js'); }}"></script>
 <script src="{{ asset('/js/bootstrap.min.js'); }}"></script>
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCsbzuJDUEOoq-jS1HO-LUXW4qo0gW9FNs&sensor=false"></script>
-<script src="{{ asset('/js/map.js'); }}"></script>
 <script src="{{ asset('/js/pxgradient-1.0.2.jquery.js'); }}"></script>
 <script src="/js/jquery.datetimepicker.js"></script>
