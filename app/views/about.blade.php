c5d6c4a413d0a3fafa79ead1980170720aeb4a00
Joshua Mays <joshua.r.mays@gmail.com>
2014-11-03 09:59:34 -0600

    fix for nav

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -20,52 +20,7 @@
         <![endif]-->
     </head>
     <body>
-      <!--navbar-->
-      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/activities">Seek City</a>
-        </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class='active'>
-                    {{ link_to_action('HomeController@index', 'Home'); }}
-                </li>
-                <li>
-                    {{ link_to_action('HomeController@about', 'About'); }}
-                </li>
-                @if (Auth::check())
-                <li>
-                    {{ link_to_action('ActivitiesController@create', 'New Activity'); }}
-                </li>
-                <li>
-                    {{ link_to_action('UsersController@getLogout', 'Log Out'); }}
-                </li>
-                <li>
-                    <a href="/categories/manage">Manage Categories</a>
-                </li>
-                <li>
-                    <a href="/moods/manage">Manage Moods</a>
-                </li>
-                @else
-                <li>
-                    {{ link_to_action('UsersController@getCreate', 'Sign Up') }}
-                </li>
-                <li>
-                    {{ link_to_action('UsersController@getLogin', 'Log In'); }}
-                </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</nav>
-<!-- /.navbar -->
+      @include('partials.navbar')
         <div class="panel"> <!--Panels for content -->
             <div class="album"> <!-- Album panel -->
                 <span class="closeicon"><i class="icon-remove"></i></span>
